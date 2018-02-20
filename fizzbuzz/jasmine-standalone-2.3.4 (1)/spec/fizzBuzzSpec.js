e8cb6ad7ac456ff2ee06753bb529e5c33c75b0be
Daniel Keen <keendaniel01@gmail.com>
2018-02-20 14:37:42 +0000

    Finished fizzbuzz functionality

--- fizzbuzz/jasmine-standalone-2.3.4 (1)/spec/fizzBuzzSpec.js
+++ fizzbuzz/jasmine-standalone-2.3.4 (1)/spec/fizzBuzzSpec.js
@@ -1,21 +1,42 @@
 describe('FizzBuzz', function() {
-
-
   var fizzbuzz;
 
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleBy(3, 3)).toBe(true);
     });
-    it ('returns false when not divisible by 3', function(){
+    it('returns false when not divisible by 3', function() {
       expect(fizzbuzz.isDivisibleBy(1, 3)).toBe(false);
     });
-    it ('returns true when divisible by 5', function(){
+    it('returns true when divisible by 5', function() {
       expect(fizzbuzz.isDivisibleBy(5, 5)).toBe(true);
     });
   });
 
+  describe('outputs number or fizzbuzz string', function() {
+    fizzbuzz = new FizzBuzz();
 
+    it('returns the number back to us', function() {
+      expect(fizzbuzz.output(1)).toEqual(1);
+    });
+
+    it('returns Fizz when number is divisible by 3', function() {
+      expect(fizzbuzz.output(3)).toEqual('Fizz');
+    });
+
+    it('returns Buzz when number is divisible by 5', function() {
+      expect(fizzbuzz.output(5)).toEqual('Buzz');
+    });
+
+    it('returns FizzBuzz when number is divisible by 15', function() {
+      expect(fizzbuzz.output(15)).toEqual('FizzBuzz');
+    });
+  });
+
+  // describe('print', function() {
+  //   it('prints numbers up to the numbers passed into it', function() {
+  //
+  //   })
+  // });
 });
--- fizzbuzz/jasmine-standalone-2.3.4 (1)/src/fizzBuzz.js
+++ fizzbuzz/jasmine-standalone-2.3.4 (1)/src/fizzBuzz.js
@@ -1,5 +1,22 @@
-var FizzBuzz = function (){};
+var FizzBuzz = function() {};
 
-FizzBuzz.prototype.isDivisibleBy = function(number, divisor){
+FizzBuzz.prototype.isDivisibleBy = function(number, divisor) {
   return number % divisor === 0;
+};
+
+FizzBuzz.prototype.output = function(number) {
+  if (this.isDivisibleBy(number, 15)) {
+    return 'FizzBuzz';
+  } else if (this.isDivisibleBy(number, 3)) {
+    return 'Fizz';
+  } else if (this.isDivisibleBy(number, 5)) {
+    return 'Buzz';
+  }
+  return number;
+};
+
+FizzBuzz.prototype.print = function(number) {
+  for (var i = 1; i < number; i++) {
+    console.log(this.output(i));
+  }
 };
